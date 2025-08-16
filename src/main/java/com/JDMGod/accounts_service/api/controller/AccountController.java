426e7769dcabe37c12d801986f134d4899b976e8
Al Amin Bin Shafiq <binshafiqalamin@gmail.com>
2025-08-16 08:59:30 -0400

    after editing

--- src/main/java/com/JDMGod/accounts_service/api/controller/AccountController.java
+++ src/main/java/com/JDMGod/accounts_service/api/controller/AccountController.java
@@ -5,26 +5,51 @@ import com.JDMGod.accounts_service.api.dto.AccountCreateRequest;
 import com.JDMGod.accounts_service.model.Account;
 import com.JDMGod.accounts_service.service.AccountService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
+    @Autowired
     private final AccountService accountService;
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
 
-    @PostMapping("/{id}")
+    @PostMapping("/saveAccount")
     public ResponseEntity<Account> createAccount(@RequestBody @Valid AccountCreateRequest accountCreateRequest) {
         return ResponseEntity.ok(accountService.create(accountCreateRequest));
 
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public ResponseEntity<Account> get(@PathVariable Long id){
         return ResponseEntity.ok(accountService.get(id));
     }
+
+
+    @GetMapping(params = "email")
+    public ResponseEntity<Account> getByEmail(@RequestBody String email){
+        return ResponseEntity.ok(accountService.getByEmail(email));
+
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Account>> getAll(){
+        return ResponseEntity.ok(accountService.list());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Account> delete(@PathVariable Long id){
+        accountService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
